05ff579c40c9c015018714d7f3cd9456ec3dc50c
Ashley Muncaster <ashley@atbweb.co.uk>
2016-07-22 22:21:29 +0100

    Initial version ready for submit.

---
+++ Assets/Scripts/LightsOutButton.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class LightsOutButton : MonoBehaviour
+{
+    public KMSelectable Selectable = null;
+    public KMAudio Audio = null;
+    public Animator Animator = null;
+    public LightsOutButton[] AdjacentButtons = null;
+
+    public bool CanInteract = false;
+    public bool LitState = false;
+
+    private void OnEnable()
+    {
+        Selectable.OnInteract += OnInteract;
+    }
+
+    private void OnDestroy()
+    {
+        Selectable.OnInteract -= OnInteract;
+    }
+
+    private bool OnInteract()
+    {
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+        if (!CanInteract)
+        {
+            return false;
+        }
+
+        InvertState();
+        foreach (LightsOutButton adjacentButton in AdjacentButtons)
+        {
+            adjacentButton.InvertState();
+        }
+
+        return false;
+    }
+
+    public void InvertState()
+    {
+        SetState(!LitState);
+    }
+
+    public void SetState(bool litState)
+    {
+        if (LitState == litState)
+        {
+            return;
+        }
+
+        LitState = litState;
+        Animator.SetTrigger(LitState ? "Lit" : "Unlit");
+    }
+}
---
+++ Assets/Scripts/LightsOutModule.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class LightsOutModule : MonoBehaviour
+{
+    public KMSelectable Selectable;
+    public KMNeedyModule NeedyModule;
+
+    private LightsOutButton[] ChildButtons
+    {
+        get
+        {
+            return GetComponentsInChildren<LightsOutButton>();
+        }
+    }
+
+    private bool AllLightsOff
+    {
+        get
+        {
+            return ChildButtons.All((x) => !x.LitState);
+        }
+    }
+
+    private void Start()
+    {
+        NeedyModule.OnNeedyActivation += OnActivate;
+        NeedyModule.OnNeedyDeactivation += OnDeactivate;
+
+        foreach (KMSelectable selectable in Selectable.Children)
+        {
+            selectable.OnInteract += OnButtonInteract;
+        }
+	}
+
+    private void OnDestroy()
+    {
+        NeedyModule.OnNeedyActivation -= OnActivate;
+        NeedyModule.OnNeedyDeactivation -= OnDeactivate;
+
+        foreach (KMSelectable selectable in Selectable.Children)
+        {
+            selectable.OnInteract -= OnButtonInteract;
+        }
+    }
+
+    private void OnActivate()
+    {
+        while (AllLightsOff)
+        {
+            RandomizeLights();
+        }
+
+        EnableLights(true);
+    }
+
+    private void OnDeactivate()
+    {
+        if (!AllLightsOff)
+        {
+            NeedyModule.HandleStrike();
+        }
+
+        ResetLights();
+        EnableLights(false);
+    }
+
+    private bool OnButtonInteract()
+    {
+        if (AllLightsOff)
+        {
+            NeedyModule.HandlePass();
+            EnableLights(false);
+        }
+
+        return false;
+    }
+
+    private void ResetLights()
+    {
+        foreach (LightsOutButton button in ChildButtons)
+        {
+            button.SetState(false);
+        }
+    }
+
+    private void RandomizeLights()
+    {
+        foreach (LightsOutButton button in ChildButtons)
+        {
+            button.SetState(Random.Range(0, 2) == 1);
+        }
+    }
+
+    private void EnableLights(bool enable)
+    {
+        foreach (LightsOutButton button in ChildButtons)
+        {
+            button.CanInteract = enable;
+        }
+    }
+}
